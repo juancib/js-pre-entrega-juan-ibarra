1c95150a207d18b526ba3fe260253b3605cc7bca
juanIbarra <juans94.ji@gmail.com>
2023-10-19 20:06:17 -0300

    entrega final juan ibarra

--- js/main.js
+++ js/main.js
@@ -1,32 +1,89 @@
-/*
-
-casa de apuestas deportivas
-
-se mostrara 3 opciones: (1) Equipo A, (2) Equipo B y (3) Empate con su cuota de pago al azar
-
-se le pedira al usuario que ingrese la opción por la cual quiera apostar
-
-luego se le pedira cuantos puntos quiere apostar
-
-se genera un resultado al azar
-
-se informa si el usuario ganó o perdió
-
-*/
-
 //generar valores
-let login = [{user: "juan94",pass: "juan1234"},{user: "admin",pass: "admin"},{user: "invitado",pass:"1234"}];
-//valida que el usuario y la contraseña esten correctas con el array objeto
+let cuotaA = 0;
+let cuotaB = 0;
+let cuotaE = 0;
+let apuesta = 0;
+let ganancia = 0;
+let resultadoGanador = 0;
+let puntos = 10000;
+let cuotaequipo1 = document.getElementById ("cuotaequipo1");
+let cuotaempate = document.getElementById ("cuotaempate");
+let cuotaequipo2 = document.getElementById ("cuotaequipo2");
+let mensaje = document.getElementById ("resultado");
 
-function hacerLogin(){
-    usuario = document.getElementById("user").value;
-    contrasenia = document.getElementById("pass").value;
+//genera un valor aleatorio de cada cuota
+function cuotaAleatoria(minimo, maximo, decimales) {
+    let precision = Math.pow(10, decimales);
+    minimo = minimo*precision;
+    maximo = maximo*precision;
+    cuotaA = Math.floor(Math.random()*(maximo-minimo+1) + minimo) / precision;
+    cuotaB = Math.floor(Math.random()*(maximo-minimo+1) + minimo) / precision;
+    cuotaE = Math.floor(Math.random()*(maximo-minimo+1) + minimo) / precision;
+}
 
-    if(login.some(e => e.user === usuario) && login.some(e => e.pass === contrasenia)){
-        window.location.href= "apuesta.html";
+function mostrarCuotas(){
+    cuotaequipo1.innerHTML = "("+cuotaA+")";
+    cuotaempate.innerHTML = "("+cuotaE+")";
+    cuotaequipo2.innerHTML = "("+cuotaB+")";
+}
+
+//genera de forma aleatoria una de las 3 opciones la cual será la ganadora
+function opcionGanadora (min, max){
+    resultadoGanador = Math.floor((Math.random() * (max - min + 1)) + min);
+}
+
+//le pide al usuario que elija una opción y la cantidad de puntos a apostar y determina si el usuario gana o pierde
+function betEquipo1(){
+    let equipo1 = document.getElementById("equipo1").value;
+    if (resultadoGanador == 1) {
+        ganancia = equipo1 * cuotaA;
+        mensaje.innerHTML = "Felicitaciones ganaste "+ ganancia + " puntos";
     }else{
-        location.reload();
+        mensaje.innerHTML = "Perdiste :´(";
     }
 }
 
+function betEmpate(){
+    let empate = document.getElementById("empate").value;
+    if (resultadoGanador == 2) {
+        ganancia = empate * cuotaE;
+        mensaje.innerHTML = "Felicitaciones ganaste "+ ganancia + " puntos";
+    }else{
+        mensaje.innerHTML = "Perdiste :´(";
+    }
+    
+}
+
+function betEquipo2(){
+    let equipo2 = document.getElementById("equipo2").value;
+    if (resultadoGanador == 3) {
+        ganancia = equipo2 * cuotaB;
+        mensaje.innerHTML = "Felicitaciones ganaste "+ ganancia + " puntos";
+    }else{
+        mensaje.innerHTML = "Perdiste :´(";
+    }
+    
+}
+//API
+
+let url = 'https://jsonplaceholder.typicode.com/users/';
+        fetch(url)
+            .then( response => response.json() )
+            .then( data => mostrarData(data) )
+            .catch( error => console.log(error) )
+
+        const mostrarData = (data) => {
+            console.log(data)
+            let body = ""
+            for (var i = 0; i < data.length; i++) {      
+               body+=`</td><td>${data[i].username}</td><td>${data[i].address.zipcode}</td></tr>`
+            }
+            document.getElementById('data').innerHTML = body
+            //console.log(body)
+        }
+
+
 //iniciar
+cuotaAleatoria(0,30,2);
+mostrarCuotas();
+opcionGanadora (1,3);
